4d77b20c3f786d15eca401e6a3bb2eafe7b36ca7
DazeCake <DazeCake@gmail.com>
2021-02-01 14:30:11 +0800

    build

---
+++ src/main/java/moe/dazecake/eco/EconomicHttpApiApplication.java
@@ -0,0 +1,13 @@
+package moe.dazecake.eco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomicHttpApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EconomicHttpApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/moe/dazecake/eco/EconomicHttpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.dazecake.eco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomicHttpApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
