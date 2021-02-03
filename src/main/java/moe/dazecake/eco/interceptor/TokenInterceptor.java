f673882b99c411f6247fcff73998b038a700b7cc
DazeCake <DazeCake@gmail.com>
2021-02-03 11:35:53 +0800

    add interceptor

---
+++ src/main/java/moe/dazecake/eco/interceptor/TokenInterceptor.java
@@ -0,0 +1,34 @@
+package moe.dazecake.eco.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class TokenInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        Object token = session.getAttribute("token");
+
+        if (token != null) {
+            return true;
+        }
+
+        response.setStatus(404);
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
