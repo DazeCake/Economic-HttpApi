6bffc80fa45f3ebc679ad41a914f56d989433c14
DazeCake <DazeCake@gmail.com>
2021-02-03 14:36:06 +0800

    update config

--- src/main/java/moe/dazecake/eco/config/WebConfig.java
+++ src/main/java/moe/dazecake/eco/config/WebConfig.java
@@ -9,7 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new TokenInterceptor())
-                .addPathPatterns("/**")
-                .excludePathPatterns("/api/getHomePage", "/api/getArticles", "/api/search");
+                .addPathPatterns("/**");
     }
 }
